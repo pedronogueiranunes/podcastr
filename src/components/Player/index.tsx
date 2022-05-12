311babc1522f47a410766a5a8f2155075fc5771b
Pedro Nogueira <pedronogueiranunes@gmail.com>
2022-05-11 21:20:16 -0300

    changed project language to typescript

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+
+export function Player() {
+
+    
+    return (
+        <div>
+
+        </div>>
+    );
+}
