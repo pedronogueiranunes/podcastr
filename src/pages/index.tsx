4c81e14c9ac3d301f6ea5e321c127d8f13c02a2c
Pedro Nogueira <pedronogueiranunes@gmail.com>
2022-05-23 08:40:39 -0300

    Addicionada dummy data de episodios em server.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,70 @@
-import { Header } from "../components/Header";
+//SPA
 
+/*
 
-export default function Home() {
+ useEffect(() => {
+    fetch("http://localhost:3333/episodes")
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []);
+
+*/
+
+import { useEffect } from "react"
+
+//SSR
+
+/*
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
+
+*/
+
+//SSG
+
+/* 
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
+}
+
+*/
+
+export default function Home(props) {
+
   return (
-    <h1>index</h1>
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
